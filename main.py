43f3fd92ce518edc41912a3fe1c9c8154e1c3c7e
fran <franobertozarazaga@gmail.com>
2021-12-21 01:24:52 -0300

    bf

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from Config import THREADS
 from Binance import WS
 from UserInterface import SOCKETIO, APP
@@ -11,13 +13,15 @@ warnings.filterwarnings("ignore")
 WS.subscribe(BOT)
 TESTER.subscribe(SOCKETIO)
 
+time = datetime.datetime.now()
+THREADS.update({'iu':Thread(target=SOCKETIO.run, kwargs={"app": APP}, name='iu')})
+THREADS.update({'tester': Thread(target=TESTER.run, name='tester')})
 THREADS.update({'bot':Thread(target=WS.run, name='bot')})
 #THREADS.update({'telegram':Thread(target=TELEGRAM.run, name='telegram')})
-#THREADS.update({'iu':Thread(target=SOCKETIO.run, kwargs={"app": APP}, name='iu')})
-THREADS.update({'tester': Thread(target=TESTER.run, name='tester')})
 
 for thread in THREADS.values():
     thread.start()
+    print(datetime.datetime.now()-time)
 
 for thread in THREADS.values():
     thread.join()
