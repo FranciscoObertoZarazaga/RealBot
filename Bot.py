e09b586a3f6817bc741916d838a8e0c6bcf3b71f
fran <franobertozarazaga@gmail.com>
2022-02-12 23:17:51 -0300

    bf

--- Bot.py
+++ Bot.py
@@ -21,7 +21,7 @@ class Bot:
         points += SqueezeStrategy(kline)
         status = self.get_last_trade()
         self.notify(status)
-        if status and random.randint(5) == 1:
+        if status and random.randint(0, 5) == 1:
             self.all_set_stop_loss()
 
         if points > 0:
