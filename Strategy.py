f96fbc83c0a6f8154e04afe9e957ce6b221a0ffa
Fran <franobertozarazaga@gmail.com>
2023-05-29 11:18:18 -0300

    Modificacion de Estrategia

--- Strategy.py
+++ Strategy.py
@@ -1,7 +1,7 @@
 def squeeze_strategy(df, n=-1):
     sm = df['sm']
-    is_min = sm[n] > sm[n - 1] and sm[n - 2] > sm[n - 1]
-    is_max = sm[n] < sm[n - 1] and sm[n - 2] < sm[n - 1]
+    is_min = sm[n] > sm[n - 1] and sm[n - 2] > sm[n - 1] and sm[n-1] < 0
+    is_max = sm[n] < sm[n - 1] and sm[n - 2] < sm[n - 1] and sm[n-1] > 0
     if is_min:
         return 1
     if is_max:
