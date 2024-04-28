acb8eb7846073a3db2dd0d79dbbd0371ad7fb3e6
Francisco Oberto <franobertozarazaga@gmail.com>
2024-04-28 20:34:20 -0300

    [master] SF

---
+++ Config.py
@@ -0,0 +1,13 @@
+from binance.enums import *
+
+ASSET = 'BTC'
+FIAT = 'USDT'
+SYMBOL = ASSET + FIAT
+IS_REAL_TRADER = False
+IS_BACKTEST = False
+INTERVALS = [KLINE_INTERVAL_1MINUTE]
+THREADS = dict()
+PUBLIC_KEY = ''  # PUBLIC_KEY
+SECRET_KEY = ''  # SECRET_KEY
+TELEGRAM_ID = []  # List of Telegram chat id
+
